5bc8aef7aa7fdb1fcf8ccb646c03a1c2bba4c337
mozow01 <78300414+mozow01@users.noreply.github.com>
2025-07-03 16:15:39 +0200

    Add files via upload

--- cat_CCC_eq.v
+++ cat_CCC_eq.v
@@ -28,6 +28,13 @@ Class CartClosedCat (C : Category) := CartClosedCat_mk {
   Prod_mor : forall {x y z} (f:x → y) (g:x → z), x → Prod_obj y z;
   First {x y} : (Prod_obj x y) → x;
   Second {x y} : (Prod_obj x y) → y;
+
+  (*           First              Second
+     A_1 <----------- A_1 x A_2 -------------> A_2
+          \               |               /
+         f_1  \  f_1 x f_2|           /   f_2
+                 \       \|/    /
+                          C                            *)
   prod_ax_1 : forall {x y z} (f : x → y) (g : x → z), 
     ((First ∘ (Prod_mor f g)) = f);
   prod_ax_2 : forall {x y z} (f : x → y) (g : x → z),((Second ∘ (Prod_mor f g)) = g);
@@ -38,6 +45,13 @@ Class CartClosedCat (C : Category) := CartClosedCat_mk {
   Exp_obj : Obj -> Obj -> Obj;
   Exp_app : forall {y z}, (Prod_obj (Exp_obj z y) y) → z;
   Lam : forall {x y z} (g : (Prod_obj x y) → z), x → (Exp_obj z y);
+  
+  (* C                     C x A
+     |                       |    \ 
+     | λ f          λf ∏ Id_A|            \ f
+    \|/                     \|/                    \
+     B^A                 B^A x A ----------------------> B 
+                                         eval                      *)
   exp_ax : forall {x y z} (g : ((Prod_obj x y) → z)), 
     (Exp_app ∘ (Prod_mor ((Lam g) ∘ First) ((Id y) ∘ Second))) = g;
   unique_exp : forall {x y z} (h : x → Exp_obj z y),
@@ -56,6 +70,8 @@ type_scope.
 
 
 Definition Isomophism {C : Category} {A B : Obj} (f : Hom A B) := (exists g : Hom B A, ((f ∘ g) = Id B ) /\ ((g ∘ f) = Id A )). 
+
+
 
 
 
